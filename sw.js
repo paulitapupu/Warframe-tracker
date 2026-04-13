a80efb90f36b4a86666810af58d2ea9386d311fa
paulitapupu <paulitapupu@gmail.com>
2026-04-13 00:53:58 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'wf-picker-v1';
+const URLS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+});
